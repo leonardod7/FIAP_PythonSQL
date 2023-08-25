c0857f8407e5e4ec76f70827c4c9357fe593bfe0
leonardod7 <107505958+leonardod7@users.noreply.github.com>
2023-08-24 21:26:51 -0300

    Adicionando código inicial

---
+++ Integracao_Python_SQL/Projeto_Livraria_SQL/model/categoria.py
@@ -0,0 +1,25 @@
+class Categoria:
+
+    def __init__(self, id: int, nome: str):
+        self.__id: int = id
+        self.__nome: str = nome
+
+    @property
+    def id(self) -> int:
+        return self.__id
+    
+    @id.setter
+    def id(self, id: int):
+        self.__id = id
+
+    @property
+    def nome(self) -> str:
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome
+
+    @staticmethod # usando o static permite que não seja preciso instanciar o objeto
+    def depositar():
+        return "Opa"
